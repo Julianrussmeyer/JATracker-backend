31ee69aacd2160bb2beb947da3b62bffd6e39037
Theodoros Mitropoulos <theodor.mitropoulos@yahoo.com>
2026-06-13 12:52:18 +0200

    Change

--- server.js
+++ server.js
@@ -1,28 +1,21 @@
-import jsonServer from "json-server";
+require("dotenv").config();
+const jsonServer = require("json-server");
+const morgan = require("morgan");
 
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const PORT = process.env.PORT || 8080;
 
-// CORS
+server.use(middlewares);
+server.use(morgan("dev"));
 server.use((req, res, next) => {
+  // Middleware to disable CORS
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "*");
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, PATCH, DELETE, OPTIONS",
-  );
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
   next();
 });
-
-server.use(middlewares);
-server.use(jsonServer.bodyParser);
 server.use(router);
 
 server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
+  console.log(`JSON Server is running at port ${PORT}`);
 });
